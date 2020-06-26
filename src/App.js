57f7d72c866e98d2d7abc1faa342bd551266f409
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-06-26 17:51:58 +0900

    Rudux 없는 React - 컴포넌트 구조 만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import AddNumberRoot from "./components/AddNumberRoot";
+import DisplayNumberRoot from "./components/DisplayNumberRoot";
 
 function App() {
   return (
     <div className="App">
-      Hello, World!
+      <h1>Root</h1>
+      <AddNumberRoot></AddNumberRoot>
+      <DisplayNumberRoot></DisplayNumberRoot>
     </div>
   );
 }
