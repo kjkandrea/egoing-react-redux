02e39071e6990a7fc4007997ac3645ac8a8c7536
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-06-30 19:05:20 +0900

    Update AddNumber.jsx

--- src/containers/AddNumber.jsx
+++ src/containers/AddNumber.jsx
@@ -2,7 +2,7 @@ import AddNumber from "../components/AddNumber";
 import {connect} from 'react-redux';
 function mapReduxDispatchToReactProps(dispatch) {
   return {
-    onClick: (size) => dispatch({type:'INCREMENT', size:size})
+    onClick: size => dispatch({type:'INCREMENT', size:size})
   }
 }
 export default connect(null, mapReduxDispatchToReactProps)(AddNumber);
